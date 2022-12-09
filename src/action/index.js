7c7817bcec870e97435da99415fec9104cf56454
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-12-09 23:27:13 +0900

    Initial Commit

--- src/action/index.js
+++ src/action/index.js
@@ -36,9 +36,7 @@ export function getUserAuth() {
 export function signInAPI() {
 	return (dispatch) => {
 		auth.signInWithPopup(provider)
-			.then((payload) => {
-				console.log(payload);
-			}) //dispatch(setUser(payload.user)))
+			.then((payload) => dispatch(setUser(payload.user)))
 			.catch((err) => alert(err.message));
 	};
 }
